11a05940c23503a9d7c5897774c8278b0d2408e9
Narknon <narknon0@gmail.com>
2022-09-06 16:07:42 -0400

    add game target

---
+++ Source/KOFXV.Target.cs
@@ -0,0 +1,12 @@
+﻿using UnrealBuildTool;
+
+public class KOFXVTarget : TargetRules {
+	public KOFXVTarget(TargetInfo Target) : base(Target) {
+		Type = TargetType.Game;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+		ExtraModuleNames.AddRange(new string[] {
+			"GameProject",
+			"GGPO",
+		});
+	}
+}
